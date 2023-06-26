a864347e0554795e4a75948934ae21d0ba165f9e
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-06-26 08:50:43 +0000

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,6 @@ import Head from 'next/head';
 import { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
 
-export default function IndexPage() {
-    return (
-        <>
-            <Head>
-                <title>Index page title</title>
-            </Head>
-            <div>
-                <p>Index page content</p>
-            </div>
-        </>
-    )
-}
 
 // const App = ({ Component, pageProps }) => {
  
