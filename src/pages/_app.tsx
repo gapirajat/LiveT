a5064538115c2d3c0b3d0b5a21561828de6b6ee2
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-06-26 08:14:32 +0000

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,35 +2,56 @@ import React from 'react';
 import '../styles/global.css';
 import Head from 'next/head';
 import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
-declare global {
-  interface Window {
-    dataLayer: any[];
-  }
-} 
 
-const App = ({ Component, pageProps }) => {
+// const App = ({ Component, pageProps }) => {
  
-  const inputRef = React.useRef<HTMLInputElement>(null);
+//   const inputRef = React.useRef<HTMLInputElement>(null);
 
+//   const onClickAnywhere = () => {
+//     inputRef.current.focus();
+//   };
+
+//   return (
+//     <>   
+//       <Head>
+        
+//         <meta
+//           name="viewport"
+//           content="initial-scale=1.0, width=device-width"
+//           key="viewport"
+//           maximum-scale="1"
+//         />
+//       </Head>
+
+//       <div
+//         className="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base"
+//         onClick={onClickAnywhere}
+//       >
+//         <main className="bg-light-background dark:bg-dark-background w-full h-full p-2">
+//           <Component {...pageProps} inputRef={inputRef} />
+//         </main>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+function MyApp({ Component, pageProps }) {
+  
+  const inputRef = React.useRef<HTMLInputElement>(null);
   const onClickAnywhere = () => {
     inputRef.current.focus();
   };
-
+  useEffect(() => {
+      TagManager.initialize({ gtmId: 'G-FEJ1TFPMGJ' });
+  }, []);
   return (
     <>   
       <Head>
         
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FEJ1TFPMGJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FEJ1TFPMGJ');
-</script>
-
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width"
@@ -47,8 +68,6 @@ const App = ({ Component, pageProps }) => {
           <Component {...pageProps} inputRef={inputRef} />
         </main>
       </div>
-    </>
-  );
+    </>);
 };
-
-export default App;
+export default MyApp
