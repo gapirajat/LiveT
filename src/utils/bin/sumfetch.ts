bf34d952d4d84e0dd50fb93b1e698a46a38d889e
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2023-08-02 19:45:13 +0000

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -15,10 +15,10 @@ const sumfetch = async (args: string[]): Promise<string> => {
  @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
   @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-     @@@                        @@@         -----------
-        @@@                  @@@ @@          DONATE 
-         @|  @@@@@@@@@@@@@@@@   @@           
-         @|                      @@          
+     @@@                        @@@         
+        @@@                  @@@ @@          
+         @|  @@@@@@@@@@@@@@@@   @@           
+         @|                      @@          
          
        
 
@@ -36,10 +36,10 @@ const sumfetch = async (args: string[]): Promise<string> => {
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   
 
 `;
   }
