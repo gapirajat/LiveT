5fc66d48a69166ee6e71ee68dbec4798e96fdc8f
gapirajat <rajat.shinde201@pccoepune.org>
2024-06-14 11:56:24 +0000

    gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,18 +6,25 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WJPTCR3F');</script>
+          {/* Global Site Tag (gtag.js) - Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WJPTCR3F');
+              `,
+            }}
+          />
         </Head>
         <body>
           <noscript
             dangerouslySetInnerHTML={{
               __html: `
-                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJPTCR3F"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
               `,
             }}
           />
