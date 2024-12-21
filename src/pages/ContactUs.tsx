42f8e2c80286769c9a9e1f8ae8613133790a36a6
gapirajat <rajatshinde01@gmail.com>
2024-12-22 01:32:02 +0530

    terms

---
+++ src/pages/ContactUs.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className="h-full p-8 border-2 border-light-yellow dark:border-dark-yellow flex flex-col overflow-auto">
+        <h1 className="text-xl">Contact Us</h1>
+      Last updated on 22-12-2024 01:23:59 
+      <br />
+      You may contact us using
+      the information below: Merchant Legal entity name: RAJAT RAHUL SHINDE
+      Registered Address: Flat 4, Soham Residency, Link Road, Chinchwad, Pune,
+      Maharashtra, PIN: 411033 Operational Address: Flat 4, Soham Residency,
+      Link Road, Chinchwad, Pune, Maharashtra, PIN: 411033 Telephone No:
+      9518771694 E-Mail ID: rajats.bn@gmail.com
+    </div>
+  );
+};
+
+export default ContactUs;
